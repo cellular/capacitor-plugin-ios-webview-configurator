f8196e9fd9b460b186a29b79ee640542750b2a0c
Dario Lehmhus <dario.lehmhus@ffw.com>
2025-08-25 15:19:18 +0200

    chore: update to latest versions and add error logs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { registerPlugin } from '@capacitor/core';
+import { registerPlugin } from "@capacitor/core";
 
 export interface WebviewConfiguratorPlugin {
   setBackForwardNavigationGestures(options: { enable: boolean }): Promise<void>;
@@ -6,22 +6,34 @@ export interface WebviewConfiguratorPlugin {
   setWebviewBounce(options: { enable: boolean }): Promise<void>;
 }
 
-export const WebviewConfiguratorPlugin = registerPlugin<WebviewConfiguratorPlugin>('WebviewConfiguratorPlugin');
+export const WebviewConfiguratorPlugin =
+  registerPlugin<WebviewConfiguratorPlugin>("WebviewConfiguratorPlugin");
 
 export function setBackForwardNavigationGestures(enable: boolean) {
   try {
     WebviewConfiguratorPlugin.setBackForwardNavigationGestures({ enable });
-  } catch (error) {}
+  } catch (error) {
+    console.error("Error setting back forward navigation gestures", error);
+  }
 }
 
 export async function getBackForwardNavigationGesturesState(): Promise<boolean> {
   try {
-    return (await WebviewConfiguratorPlugin.getBackForwardNavigationGesturesState()).data;
-  } catch (error) {}
+    return (
+      await WebviewConfiguratorPlugin.getBackForwardNavigationGesturesState()
+    ).data;
+  } catch (error) {
+    console.error(
+      "Error getting back forward navigation gestures state",
+      error
+    );
+  }
 }
 
 export function setWebviewBounce(enable: boolean) {
   try {
     WebviewConfiguratorPlugin.setWebviewBounce({ enable });
-  } catch (error) {}
+  } catch (error) {
+    console.error("Error setting webview bounce", error);
+  }
 }
